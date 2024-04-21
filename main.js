5e8aa5ac3e04329480b1b8b624cfa2a58a9d5ede
Shoaib-ur-Rehman <160622745+shoaiburrehman2211@users.noreply.github.com>
2024-04-22 02:31:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+/*Store your favorite number in a variable. Then, using that variable, create a message that
+reveals your favorite number. Print that message.
+*/
+var favNum = 7;
+var message = "My favorite Number is ".concat(favNum);
+console.log(message);
